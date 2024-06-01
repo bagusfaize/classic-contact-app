12721e156587b01f728ea11a5da2490cb493b69c
bagus faize <bagusfaize@gmail.com>
2024-06-01 22:23:28 +0700

    rework header

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
+import { Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
-type MenuItem = Required<MenuProps>['items'][number];
+type NavigationProps = {
+    onAddContact: () => void;
+}
 
-const items: MenuItem[] = [
-    {
-        label: 'Home',
-        key: 'home',
-    },
-    {
-        label: 'Contact',
-        key: 'contact',
-    },
-];
+export default function Navigation({
+    onAddContact
+}: NavigationProps) {
 
-export default function Navigation() {
-    const [current, setCurrent] = useState('contact');
-
-    const onClick: MenuProps['onClick'] = (e) => {
-        setCurrent(e.key);
-    };
-
-    return <Menu style={{marginBottom:'1.5rem'}} onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+    return (
+        <div className='flex justify-between items-center bg-white px-5 py-4 mb-5 shadow-sm rounded-xl'>
+            <div className='flex items-center'>
+                <h1 className="flex items-center font-semibold text-lg  bg-blue-500 h-10 px-2 text-white rounded-s-lg">
+                    OBS
+                </h1>
+                <span className='text-sm flex items-center bg-yellow-500 h-10 px-2 text-white rounded-e-lg'>Contact Manager</span>
+            </div>
+            <Button type="primary" onClick={onAddContact} className='flex items-center py-4'>
+                <PlusOutlined /> 
+                <span className='hidden lg:block'>ADD CONTACT</span>
+            </Button>
+        </div>
+    )
 };
