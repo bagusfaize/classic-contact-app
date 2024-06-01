0b32eee8e250fc6826ef10e80cbd3b73ce48278d
bagus faize <bagusfaize@gmail.com>
2024-06-01 15:18:53 +0700

    mapping data to component

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,44 @@
 'use client'
-import { setContactState } from "@/store/features/contactSlices";
-import { useAppDispatch, useAppSelector } from "@/store/store";
-import { Button } from "antd";
-import Image from "next/image";
-
-const data = [
-  {
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  },
-  {
-    "id": 2,
-    "name": "Ervin Howell",
-    "username": "Antonette",
-    "email": "Shanna@melissa.tv",
-    "address": {
-      "street": "Victor Plains",
-      "suite": "Suite 879",
-      "city": "Wisokyburgh",
-      "zipcode": "90566-7771",
-      "geo": {
-        "lat": "-43.9509",
-        "lng": "-34.4618"
-      }
-    },
-    "phone": "010-692-6593 x09125",
-    "website": "anastasia.net",
-    "company": {
-      "name": "Deckow-Crist",
-      "catchPhrase": "Proactive didactic contingency",
-      "bs": "synergize scalable supply-chains"
-    }
-  },
-  {
-    "id": 3,
-    "name": "Clementine Bauch",
-    "username": "Samantha",
-    "email": "Nathan@yesenia.net",
-    "address": {
-      "street": "Douglas Extension",
-      "suite": "Suite 847",
-      "city": "McKenziehaven",
-      "zipcode": "59590-4157",
-      "geo": {
-        "lat": "-68.6102",
-        "lng": "-47.0653"
-      }
-    },
-    "phone": "1-463-123-4447",
-    "website": "ramiro.info",
-    "company": {
-      "name": "Romaguera-Jacobson",
-      "catchPhrase": "Face to face bifurcated interface",
-      "bs": "e-enable strategic applications"
-    }
-  },
-]
+import { useEffect } from "react";
+import { setContactState } from "@/app/store/slices/contactSlices";
+import { useAppDispatch, useAppSelector } from "./store/store";
+import { ContactProps } from "@/app/types/type";
+import { Col, Row } from "antd";
+import ContactCard from "./components/ContactCard";
+import Navigation from "./components/Navigation";
 
 export default function Home() {
   const contactState = useAppSelector(state => state.contact.contacts);
   const dispatch = useAppDispatch();
-  console.log('clg state', contactState);
 
-  const setContact = () => {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setContact(data)
+      })
+  }, [])
+
+  const setContact = (data: Array<ContactProps>) => {
     dispatch(setContactState(data));
   }
 
-  const clearContact = () => {
-    dispatch(setContactState([]));
-  }
-
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <Button  onClick={setContact}>
-        SET CONTACT
-      </Button>
-      <Button danger onClick={clearContact}>
-        CLEAR CONTACT
-      </Button>
+    <main className="p-10 bg-slate-100 min-h-screen">
+      <Navigation/>
+      <div className="py-10">
+        <Row gutter={[20, 20]}>
+          {contactState.map(item => {
+            return (
+              <Col key={item.id} xs={24} sm={12} md={6}>
+                <ContactCard
+                  contact={item}
+                />
+              </Col>
+            )
+          })}
+        </Row>
+      </div>
     </main>
   );
 }
