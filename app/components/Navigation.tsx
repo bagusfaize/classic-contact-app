acb1677cfd850a0aa42e4a2e9d88ee4d3cfe307a
bagus faize <bagusfaize@gmail.com>
2024-06-01 15:18:09 +0700

    add navigation component

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+    {
+        label: 'Home',
+        key: 'home',
+    },
+    {
+        label: 'Contact',
+        key: 'contact',
+    },
+];
+
+export default function Navigation() {
+    const [current, setCurrent] = useState('contact');
+
+    const onClick: MenuProps['onClick'] = (e) => {
+        setCurrent(e.key);
+    };
+
+    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
