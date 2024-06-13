cdfcd0518edebd5072b5a3cffef66b1e67bf3115
bagus faize <bagusfaize@gmail.com>
2024-06-13 17:07:09 +0700

    change wording

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -14,7 +14,7 @@ export default function Navigation({
         <div className='flex justify-between items-center bg-white px-5 py-4 mb-10 shadow-sm rounded-xl'>
             <div className='flex items-center'>
                 <h1 className="flex items-center font-semibold text-lg  bg-blue-500 h-10 px-2 text-white rounded-s-lg kanit-font">
-                    OBS
+                    Classic
                 </h1>
                 <span className='text-sm flex items-center bg-yellow-500 h-10 px-2 text-white rounded-e-lg'>Contact Manager</span>
             </div>
